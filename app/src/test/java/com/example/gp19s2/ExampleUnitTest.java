62e9cdb048962ae03568c7253879c5bfb7d39271
u6801714 <u6801714@anu.edu.au>
2019-10-14 19:12:06 +0800

    update

--- app/src/test/java/com/example/gp19s2/ExampleUnitTest.java
+++ app/src/test/java/com/example/gp19s2/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.example.gp19s2;
 
+import android.content.Context;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +12,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    Context context;
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
