f9f27f5f751741e56159c850f9f2d7ad8ac0665d
yijianfan <u6711966@anu.edu.au>
2019-10-14 17:50:52 +1100

    CAN TEST DATABASE

--- app/src/androidTest/java/com/example/gp19s2/editTest.java
+++ app/src/androidTest/java/com/example/gp19s2/editTest.java
@@ -1,6 +1,9 @@
 package com.example.gp19s2;
 
 import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
 
 import org.junit.After;
 import org.junit.Before;
@@ -21,11 +24,14 @@ import static org.junit.Assert.*;
 public class editTest {
     private String mTitle="Test";
     private String mDES = "Have a good markz!";
+    private edit medit=null;
     @Rule
     public ActivityTestRule<edit> activityRule
             = new ActivityTestRule<>(edit.class);
+
     @Before
     public void setUp() throws Exception {
+        medit=activityRule.getActivity();
     }
 
     @After
@@ -37,10 +43,43 @@ public class editTest {
         Espresso.closeSoftKeyboard();
         Espresso.onView(withId(R.id.TITLE2)).check(matches(withText(mTitle)));
     }
+    @Test
     public void user_type_des() {
         Espresso.onView(withId(R.id.DESC2)).perform(typeText(mDES));
         Espresso.closeSoftKeyboard();
         Espresso.onView(withId(R.id.DESC2)).check(matches(withText(mDES)));
+    }
+   @Test
+    public void testLauch() {
+        View view=medit.findViewById(R.id.DATE2);
+        View view2=medit.findViewById(R.id.TIME2);
+        assertNotNull(view);
+        assertNotNull(view2);
+    }
+    @Test
+    public void testInsert() {
+        String title_get="";
+        String date_get="";
+        String time_get="";
+        String Des_get="";
+
+        Context context=medit.getApplicationContext();
+        Database thisDB = new Database(context);
+        thisDB.insert("Test","10/11/2019","3:03","have a test!",null);
+        Cursor c=thisDB.getList();
+        while (c.moveToNext()){
+             title_get= c.getString(1);
+             date_get=c.getString(2);
+             time_get=c.getString(3);
+             Des_get=c.getString(4);
+
+        }
+        assertEquals("Test",title_get);
+        assertEquals("10/11/2019",date_get);
+        assertEquals("3:03",time_get);
+        assertEquals("have a test!",Des_get);
+
+
     }
 
 }
